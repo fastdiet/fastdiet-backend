d82ac4e33b0d5cb95bb7e6a58293e8513931e0be
Pablo Pardo <pardofernandezpablo@gmail.com>
2025-05-11 21:46:24 +0200

    Script to run the app

--- run_backend.ps1
+++
@@ -1,1 +0,0 @@
-Start-Process powershell -ArgumentList ".\venv\Scripts\activate; uvicorn app.api.main:app --reload --host 0.0.0.0 --port 13000  --log-level debug"
---
+++ run_backend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source venv/bin/activate
+uvicorn app.api.main:app --host 0.0.0.0 --port 13000 --reload --log-level debug
