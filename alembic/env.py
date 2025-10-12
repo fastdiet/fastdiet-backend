4e9c385c3a0d2b766a6ac2d474747657472c2c57
Pablo Pardo <84620826+pablopf197@users.noreply.github.com>
2025-10-12 15:19:39 +0200

    Update cloud build

--- alembic/env.py
+++ alembic/env.py
@@ -7,35 +7,25 @@ from alembic import context
 from app.core.config import get_settings
 from app.models import *
 
-# this is the Alembic Config object
 config = context.config
 
-# --- INICIO DE LA MODIFICACIÓN CLAVE ---
-# Comprobamos si estamos en el entorno de migración de Cloud Build
-# buscando la variable de entorno DB_HOST.
 if os.environ.get("DB_HOST"):
-    # Si estamos en el pipeline, construimos la URL dinámicamente
-    # con las variables que nos pasa el 'docker run' de Cloud Build.
     db_user = os.environ.get("DB_USER")
     db_pass = os.environ.get("DB_PASS")
     db_host = os.environ.get("DB_HOST")
     db_port = os.environ.get("DB_PORT")
     db_name = os.environ.get("DB_NAME")
     
-    # ¡AQUÍ ESTÁ EL CAMBIO! Usamos el dialecto mysql+pymysql.
     prod_database_url = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
     
     config.set_main_option('sqlalchemy.url', prod_database_url)
 else:
-    # Si no, estamos en local y usamos la configuración del .env como siempre.
     settings = get_settings()
     config.set_main_option(
         'sqlalchemy.url',
         settings.prod_database_url or settings.database_url
     )
-# --- FIN DE LA MODIFICACIÓN CLAVE ---
 
-# El resto del archivo permanece igual
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
